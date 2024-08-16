53496cb436cec257cf4c31bf6921950bdef9926e
Alia verma <98269204+Alia1234567@users.noreply.github.com>
2024-08-16 16:17:06 +0000

    notes

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,10 +1,20 @@
+// Import the mongoose library for working with MongoDB
 const mongoose = require("mongoose");
+
+// Import the colors library to add colors to console logs
 const colors = require("colors");
+
+// Define an asynchronous function to connect to the MongoDB database
 const connectDb = async () => {
   try {
+        // Attempt to connect to the MongoDB database using the URL from environment variables
     await mongoose.connect(process.env.MONGO_URL);
+
+    // If the connection is successful, log the host name of the MongoDB server with colored text
     console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
+
+    // If there is an error during connection, log the error message with a red background
     console.log(`${error}`.bgRed);
   }
 };
